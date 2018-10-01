36b45ba25c26da51a195827e910ee736bde40e95
Martin Homuth <martin.homuth@emlix.com>
2018-10-01 15:33:30 +0200

    zsh: add deployment script
    Signed-off-by: Martin Homuth <martin.homuth@emlix.com>

---
+++ zsh/deploy_zsh_files.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# zsh startup file deployment script
+
+if [ -f "/usr/local/include/common.sh" ]; then
+	. "/usr/local/include/common.sh"
+else
+	error() {
+		printf "ERROR: %s" "$@"
+	}
+fi
+
+if [ ! $UID -eq 0 ]; then
+	error "Must be executed as root"
+fi
+
+if [ ! -f /bin/zsh ]; then
+	error "zsh not available"
+fi
+
+ZSHENV="zshenv"
+ZSHENVTARGET="/home/${SUDO_USER}/.zshenv"
+ETCZSHENV="etc-zshenv"
+ETCZSHENVTARGET="/etc/zshenv"
+ZPROFILE="zprofile"
+ZPROFILETARGET="/home/${SUDO_USER}/.zprofile"
+ETCZPROFILE="etc-zprofile"
+ETCZPROFILETARGET="/etc/zprofile"
+ZSHRC="zshrc"
+ZSHRCTARGET="/home/${SUDO_USER}/.zshrc"
+ETCZSHRC="etc-zshrc"
+ETCZSHRCTARGET="/etc/zshrc"
+ZLOGIN="zlogin"
+ZLOGINTARGET="/home/${SUDO_USER}/.zlogin"
+ETCZLOGIN="etc-zlogin"
+ETCZLOGINTARGET="/etc/zlogin"
+ZSHDIR="zsh.d"
+ZSHDIRTARGET="/home/${SUDO_USER}/.zsh.d"
+OHMYZSH="oh-my-zsh"
+OHMYZSHTARGET="/home/${SUDO_USER}/.oh-my-zsh"
+
+# create array of [file in repository : file target ]
+# iterate over array
+# if target file does not exist
+# link file to the target location
+
+declare -A fileArray=(
+	[${ZSHENV}]=${ZSHENVTARGET}
+	[${ETCZSHENV}]=${ETCZSHENVTARGET}
+	[${ZPROFILE}]=${ZPROFILETARGET}
+	[${ETCZPROFILE}]=${ETCZPROFILETARGET}
+	[${ZSHRC}]=${ZSHRCTARGET}
+	[${ETCZSHRC}]=${ETCZSHRCTARGET}
+	[${ZLOGIN}]=${ZLOGINTARGET}
+	[${ETCZLOGIN}]=${ETCZLOGINTARGET}
+	[${ZSHDIR}]=${ZSHDIRTARGET}
+	[${OHMYZSH}]=${OHMYZSHTARGET}
+)
+
+for K in "${!fileArray[@]}"; do
+
+	target="${fileArray[$K]}"
+
+	if [ -z "${REMOVE}" ]; then
+
+		if [ -e "$target" -o -L "$target" ]; then
+			warning "Configuration file $target already exists"
+			continue
+		fi
+
+		ln -sv "$(pwd -P)/$K" "${fileArray[$K]}"
+	else
+		if [ -L ${target} ]; then
+			rm -v ${target}
+		else
+			warning "$target is no symbolic link"
+		fi
+	fi
+done
