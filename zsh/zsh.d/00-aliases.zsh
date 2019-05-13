44242535bcbb78a2101fffddc436b7cb2a180753
Martin Homuth <martin.homuth@emlix.com>
2019-05-13 19:33:35 +0200

    zshrc: remove reboot and shutdown aliases
    Signed-off-by: Martin Homuth <martin.homuth@emlix.com>

--- zsh/zsh.d/00-aliases.zsh
+++ zsh/zsh.d/00-aliases.zsh
@@ -12,7 +12,6 @@ alias cD='cd ~/Downloads'
 
 # System
 alias lock='xscreensaver-command --lock'
-alias reboot='sudo reboot'
 if [ -x /usr/bin/emerge ]; then
 	alias updatedb="sudo eix-update && sudo emerge --sync"
 	alias update='sudo emerge -uDNa world && sudo emerge -uDNa system'
@@ -22,7 +21,6 @@ elif [ -x /usr/bin/pacman ]; then
 else
 	alias update="echo unknown distribution"
 fi
-alias shutdown='sudo shutdown -hP now'
 alias systemctl='sudo systemctl'
 alias netctl='sudo netctl'
 alias k='exit'
