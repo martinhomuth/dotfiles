707a92c761497660923757966eb227817d604aee
Martin Homuth <martin@followthestack.tech>
2019-10-15 07:59:04 +0200

    setup_dotfiles: add i3 config
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -14,6 +14,7 @@ BRANCH=$(git branch --show-current)
 source configs/${BRANCH}
 
 # Xdefaults
+
 FILE=Xdefaults
 TGT="${HOME}/.${FILE}"
 cp ${FILE} ${TGT}
@@ -21,4 +22,15 @@ cp ${FILE} ${TGT}
 sed -i "s/\${FONTNAME}/${FONTNAME}/g" ${TGT}
 sed -i "s/\${FONTSIZE}/${FONTSIZE}/g" ${TGT}
 
+# i3
+
+FILE=i3wm/config
+TGT="${HOME}/.i3/config"
+mkdir -pv $(dirname ${TGT})
+cp ${FILE} ${TGT}
+
+sed -i "s/\${MAINMONITOR}/${MAINMONITOR}/g" ${TGT}
+sed -i "s/\${SECONDARYMONITOR}/${SECONDARYMONITOR}/g" ${TGT}
+sed -i "s/\${FONTNAME}/${FONTNAME}/g" ${TGT}
+sed -i "s/\${FONTSIZE}/${FONTSIZE}/g" ${TGT}
 
