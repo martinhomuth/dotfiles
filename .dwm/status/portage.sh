f266a399aedd51d7a0b110b106f1159643f2e869
Martin Homuth <martin@followthestack.tech>
2021-08-08 21:29:42 +0200

    dwm: status: portage updates
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- .dwm/status/portage.sh
+++ .dwm/status/portage.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "$(eix-diff | grep -c "\[U\]") updates"
+TMPFILE=/tmp/sb-portage.cache
+
+nice -n19 emerge -pquDNa @world | wc -l &> ${TMPFILE} &
+
+read -r num_updates 2>/dev/null < ${TMPFILE}
+if [ -n "$num_updates" ] && [ ! "$num_updates" = "0" ]; then
+	echo "${num_updates} updates"
+fi
