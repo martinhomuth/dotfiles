6f808e2e9751adb53b61f9a523caf42b50c82f44
Martin Homuth <martin@followthestack.tech>
2021-08-10 09:23:01 +0200

    dwm: status: update portage script
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- .dwm/status/portage.sh
+++ .dwm/status/portage.sh
@@ -2,7 +2,12 @@
 
 TMPFILE=/tmp/sb-portage.cache
 
-nice -n19 emerge -pquDNa @world | wc -l &> ${TMPFILE} &
+# check if portage is already running
+if pgrep 'emerge' >/dev/null; then
+	exit 1
+fi
+
+nice -n19 emerge -pquDNa @world | grep -c '\[ebuild' 1> ${TMPFILE} 2>/dev/null &
 
 read -r num_updates 2>/dev/null < ${TMPFILE}
 if [ -n "$num_updates" ] && [ ! "$num_updates" = "0" ]; then
