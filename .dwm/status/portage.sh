d69a4b7a455c58d8bd6827a8b3e1ebf37946fd32
Martin Homuth <martin@followthestack.tech>
2022-03-06 16:16:58 +0100

    dwm: status: update update status
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- .dwm/status/portage.sh
+++ .dwm/status/portage.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+set -x
+
 TMPFILE=/tmp/sb-portage.cache
+LOGFILE=/tmp/sb-portage.log
 
 # check if portage is already running
 if pgrep 'emerge' >/dev/null; then
 	exit 1
 fi
 
-nice -n19 emerge -pquDNa @world | grep -c '\[ebuild' 1> ${TMPFILE} 2>/dev/null &
+nice -n19 emerge -pquDNa --backtrack=100 --with-bdeps=y @world | tee ${LOGFILE} | grep -c '\[ebuild' 1> ${TMPFILE} 2>/dev/null &
 
 read -r num_updates 2>/dev/null < ${TMPFILE}
 if [ -n "$num_updates" ] && [ ! "$num_updates" = "0" ]; then
