187ff51886745f36678ba04493b3a3129bdc2349
Martin Homuth <martin.homuth@emlix.com>
2024-09-24 15:53:54 +0200

    X11: add screenlayout for devpool082
    Signed-off-by: Martin Homuth <martin.homuth@emlix.com>

---
+++ X11/.screenlayout/devpool082.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 0x0 --scale 1.0 --dpi 128 --rotate normal --output HDMI-A-0 --primary --mode 2560x1440 --pos 2560x0 --scale 1.0 --dpi 128 --output HDMI-A-1 --off --output DVI-D-0 --off
