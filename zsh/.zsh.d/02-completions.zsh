9abb34945ea8bbf56229dd6a471ac1948b03c577
Martin Homuth <martin.homuth@emlix.com>
2025-06-03 07:00:27 +0200

    feat: zsh: update comppletions
    Signed-off-by: Martin Homuth <martin.homuth@emlix.com>

--- zsh/.zsh.d/02-completions.zsh
+++ zsh/.zsh.d/02-completions.zsh
@@ -1,16 +1,21 @@
-zstyle ':completion:::::' completer _complete _approximate
+zstyle ':completion:::::' completer _extensions _complete _approximate
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.zsh-cache
+zstyle ':completion:*' menu search
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' hosts $ssh_hosts
 zstyle ':completion:my-accounts' users-hosts $my_accounts
 zstyle ':completion:other-accounts' users-hosts $other_accounts
 zstyle -e ':completion:*:approximate:*' max-errors 'reply=( $(( ($#PREFIX + $#SUFFIX) / 3 )) )'
 zstyle ':completion:*:descriptions' format "- %d -"
-zstyle ':completion:*:corrections' format "- %d - (errors %e})"
+zstyle ':completion:*:corrections' format "%F{yellow}- %d - (errors %e})%f"
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
 zstyle ':completion:*:default' list-prompt '%S%M matches%s'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*:manuals' separate-sections true
 zstyle ':completion:*:manuals.(^1*)' insert-sections true
 zstyle ':completion:*' verbose yes
 zstyle ':completion:*' file-list list=20 insert=10
+zstyle ':completion:*' file-sort change follow
+
